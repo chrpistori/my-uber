2fa95586562257bcbec14fcfcb26a917d79268c5
chrpistori <chr.pistori@gmail.com>
2023-07-02 20:20:44 -0300

    Code Smell: Comentários -> variáveis explicativas

--- backend/ride/src/main.ts
+++ backend/ride/src/main.ts
@@ -11,30 +11,26 @@ app.post("/calculate_ride", function (req, res) {
         segments.date = new Date(segments.date);
         if (segments.distance != null && segments.distance != undefined && typeof segments.distance === "number" && segments.distance > 0) {
             if (segments.date != null && segments.date != undefined && segments.date instanceof Date && segments.date.toString() !== "Invalid Date") {
-                //overnight
-                if (segments.date.getHours() >= 22 || segments.date.getHours() <= 6) {
-                        //not sunday
-                    if (segments.date.getDay() !== 0) {
+                const isOvernight = segments.date.getHours() >= 22 || segments.date.getHours() <= 6;
+                const isSunday = segments.date.getDay() === 0;
+                if (isOvernight) {
+                    if (!isSunday) {
                         price += segments.distance * 3.90;
-                        // sunday
                     } else {
                         price += segments.distance * 5;
                     }
                 } else {
-                    // sunday
-                    if (segments.date.getDay() === 0) {
+                    if (isSunday) {
                         price += segments.distance * 2.9;
                     } else {
                         price += segments.distance * 2.10;
                     }
                 }
             } else {
-                // console.log();
                 res.json({ price: -2 });
                 return;
             }
         } else {
-            // console.log(req.body.distance);
             res.json({ price: -1 });
             return;
         }
