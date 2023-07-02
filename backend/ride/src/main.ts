afd8341b08e39c2eaf7132c59cba78b309c37287
chrpistori <chr.pistori@gmail.com>
2023-07-02 20:11:00 -0300

    Corrigindo Code Smell: Linhas em Branco

--- backend/ride/src/main.ts
+++ backend/ride/src/main.ts
@@ -1,27 +1,20 @@
 // @ts-nocheck
 import express from "express";
 const app = express();
+
 app.use(express.json());
 
 // calculate ride price
 app.post("/calculate_ride", function (req, res) { 
     let price = 0;
-
     for (const segments of req.body.segments) {
-
         segments.date = new Date(segments.date);
-
         if (segments.distance != null && segments.distance != undefined && typeof segments.distance === "number" && segments.distance > 0) {
-
             if (segments.date != null && segments.date != undefined && segments.date instanceof Date && segments.date.toString() !== "Invalid Date") {
-
                 //overnight
-
                 if (segments.date.getHours() >= 22 || segments.date.getHours() <= 6) {
-
                         //not sunday
                     if (segments.date.getDay() !== 0) {
-
                         price += segments.distance * 3.90;
                         // sunday
                     } else {
@@ -30,9 +23,7 @@ app.post("/calculate_ride", function (req, res) {
                 } else {
                     // sunday
                     if (segments.date.getDay() === 0) {
-                        
                         price += segments.distance * 2.9;
-
                     } else {
                         price += segments.distance * 2.10;
                     }
@@ -44,7 +35,6 @@ app.post("/calculate_ride", function (req, res) {
             }
         } else {
             // console.log(req.body.distance);
-
             res.json({ price: -1 });
             return;
         }
--- backend/ride/test/main.test.ts
+++ backend/ride/test/main.test.ts
@@ -1,22 +1,16 @@
 import axios from "axios";
 import { response } from "express";
 
-
 test("Deve fazer o cálculo do preço de uma corrida durante o dia", async function () {
-    // given, when, then
-    // arrange, act, assert
-    
     // given or arrange
     const input = {
         segments: [
             { distance: 10, date: "2021-03-01T10:00:00" }
         ]
     };
-
     // when or act
     const response = await axios.post("http://localhost:3000/calculate_ride", input);
     const output = response.data;
-    
     // then or assert
     expect(output.price).toBe(21);
 });
