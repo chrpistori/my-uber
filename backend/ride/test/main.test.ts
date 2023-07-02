e42c5ccbe6f8f6b3a2a26fba285f4220a8aa8694
chrpistori <chr.pistori@gmail.com>
2023-07-02 19:50:33 -0300

    Adicionando novos testes

--- backend/ride/test/main.test.ts
+++ backend/ride/test/main.test.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { response } from "express";
 
 
-test("Deve fazer o cálculo do preço de uma corrida", async function () {
+test("Deve fazer o cálculo do preço de uma corrida durante o dia", async function () {
     // given, when, then
     // arrange, act, assert
     
@@ -17,4 +17,49 @@ test("Deve fazer o cálculo do preço de uma corrida", async function () {
     
     // then or assert
     expect(output.result).toBe(21);
+});
+
+test("Deve fazer o cálculo do preço de uma corrida durante a noite", async function () {
+    const input = [
+        { dist: 10, ds: "2021-03-01T23:00:00" }
+    ];
+    const response = await axios.post("http://localhost:3000/calc", input);
+    const output = response.data;
+    expect(output.result).toBe(39);
+});
+
+test("Deve fazer o cálculo do preço de uma corrida no domingo de dia", async function () {
+    const input = [
+        { dist: 10, ds: "2021-03-07T10:00:00" }
+    ];
+    const response = await axios.post("http://localhost:3000/calc", input);
+    const output = response.data;
+    expect(output.result).toBe(29);
+});
+
+test("Deve fazer o cálculo do preço de uma corrida no domingo de noite", async function () {
+    const input = [
+        { dist: 10, ds: "2021-03-07T23:00:00" }
+    ];
+    const response = await axios.post("http://localhost:3000/calc", input);
+    const output = response.data;
+    expect(output.result).toBe(50);
+});
+
+test("Deve retornar -1 se a distância for inválida", async function () {
+    const input = [
+        { dist: -10, ds: "2021-03-07T23:00:00" }
+    ];
+    const response = await axios.post("http://localhost:3000/calc", input);
+    const output = response.data;
+    expect(output.result).toBe(-1);
+});
+
+test("Deve retornar -2 se a data for inválida", async function () {
+    const input = [
+        { dist: 10, ds: "bla" }
+    ];
+    const response = await axios.post("http://localhost:3000/calc", input);
+    const output = response.data;
+    expect(output.result).toBe(-2);
 });
