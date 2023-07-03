059597ec55eb3ee3ce4a3f9b26417089fa970fe6
chrpistori <chr.pistori@gmail.com>
2023-07-03 01:18:02 -0300

    CPF: Adicionados novos testes

--- backend/ride/test/cpf.test.ts
+++ backend/ride/test/cpf.test.ts
@@ -7,8 +7,26 @@ test("Deve informar que o CPF informado é válido", function () {
     expect(result).toBeTruthy();
 });
 
-test("Deve informar que o CPF informado não é válido", function () {
+test("Deve informar que o CPF informado é inválido", function () {
     const cpf = "07959873908";
     const result = validate(cpf);
     expect(result).toBeFalsy();
+});
+
+test("Deve informar que o CPF informado é inválido porque tem todos os dígitos iguais", function () {
+    const cpf = "99999999999";
+    const result = validate(cpf);
+    expect(result).toBeFalsy();
+});
+
+test("Deve informar que o CPF informado com caracteres da máscara é um CPF válido.", function () {
+    const cpf = "635.656.769-49";
+    const result = validate(cpf);
+    expect(result).toBeTruthy();
+});
+
+test("Deve informar que o CPF informado com caracteres da máscara é um CPF inválido.", function () {
+    const cpf = "635.656.769-49";
+    const result = validate(cpf);
+    expect(result).toBeTruthy();
 });
