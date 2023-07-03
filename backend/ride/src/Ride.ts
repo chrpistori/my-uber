3a97898e1a6a54f4d43affbc38d2cfb0c5da7bfb
chrpistori <chr.pistori@gmail.com>
2023-07-02 23:03:28 -0300

    Code Smell: Números Mágicos

--- backend/ride/src/Ride.ts
+++ backend/ride/src/Ride.ts
@@ -2,6 +2,12 @@ import Segment from "./Segment";
 
 export default class Ride {
     segments: Segment[];
+    FIRST_DAY_OF_MONTH_FARE = 2;
+    OVERNIGHT_FARE = 3.9;
+    OVERNIGHT_SUNDAY_FARE = 5;
+    SUNDAY_FARE = 2.9;
+    NORMAL_FARE = 2.1;
+    MIN_PRICE = 10;
 
     constructor () {
         this.segments = [];
@@ -15,22 +21,22 @@ export default class Ride {
         let price = 0;
         for (const segment of this.segments) {
             if (segment.isFirstDayOfMonth()) {
-                price += segment.distance * 3;
+                price += segment.distance * this.FIRST_DAY_OF_MONTH_FARE;
                 continue;
             }
             if (segment.isOvernight() && !segment.isSunday()) {
-                price += segment.distance * 3.90;
+                price += segment.distance * this.OVERNIGHT_FARE;
             }
             if (segment.isOvernight() && segment.isSunday()) {
-                price += segment.distance * 5;
+                price += segment.distance * this.OVERNIGHT_SUNDAY_FARE;
             }
             if (!segment.isOvernight() && segment.isSunday()) {
-                price += segment.distance * 2.9;
+                price += segment.distance * this.SUNDAY_FARE;
             }
             if (!segment.isOvernight() && !segment.isSunday()) {
-                price += segment.distance * 2.10;
+                price += segment.distance * this.NORMAL_FARE;
             }
         }
-        return (price < 10) ? 10 : price;
+        return (price < this.MIN_PRICE) ? this.MIN_PRICE : price;
     }
 }
