810f9d8bea4773b0776ac427d743b93e2ec24b71
chrpistori <chr.pistori@gmail.com>
2023-07-02 23:25:34 -0300

    Demonstrando como seria o teste direto na api

--- backend/ride/test/api.test.ts
+++ backend/ride/test/api.test.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+axios.defaults.validateStatus = function () {
+    return true;
+};
+
 test("Deve fazer o cálculo do preço de uma corrida durante o dia", async function () {
     // given or arrange
     const input = {
@@ -12,4 +16,16 @@ test("Deve fazer o cálculo do preço de uma corrida durante o dia", async funct
     const output = response.data;
     // then or assert
     expect(output.price).toBe(21);
+});
+
+test("Se a distância for inválida, deve lançar um erro", async function () {
+    const input = {
+        segments: [
+            { distance: -10, date: "2021-03-02T10:00:00" }
+        ]
+    };
+    const response = await axios.post("http://localhost:3000/calculate_ride", input);
+    expect(response.status).toBe(422);
+    const output = response.data;
+    expect(output).toBe("Invalid Distance");
 });
