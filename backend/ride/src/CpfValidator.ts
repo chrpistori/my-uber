76912c74997baf5bbefe92525e096f0dcf415e76
christianpistori <christian.pistori@grupoboticario.com.br>
2023-07-03 18:38:28 -0300

    Removendo validações repetidas.

--- backend/ride/src/CpfValidator.ts
+++ backend/ride/src/CpfValidator.ts
@@ -10,9 +10,7 @@ export default class CpfValidator {
 
   constructor (readonly cpf: string) {
     this.cpfUtils = new CpfUtils(this.cpf);
-    if (!this.cpfUtils.isCpfValid() || this.cpfUtils.hasAllDigitsEqual()) return false;
     this.validCpf = this.cpfUtils.removeMaskCharacters();
-    if (!this.cpfUtils.isCpfValidNumber()) return false;
   }
 
   validate () {
@@ -23,8 +21,7 @@ export default class CpfValidator {
       const secondFinalDigit = this.cpfCalculator.calculateSecondFinalDigit(secondDigit, firstFinalDigit);
       return this.cpfCalculator.isVerifierDigitValid(firstFinalDigit, secondFinalDigit);
     } catch (e) {
-      console.error("Erro: " + e);
-      return false;
+      throw new Error("An error occurred while validating CPF: " + e.message);
     }
   }
 }
