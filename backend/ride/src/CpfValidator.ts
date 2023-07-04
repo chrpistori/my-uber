8570f161a0df9ba959f2a0641ccca6506d233b01
chrpistori <chr.pistori@gmail.com>
2023-07-04 01:31:47 -0300

    CPF: Code Smell(remover try catch desnecessário)

--- backend/ride/src/CpfValidator.ts
+++ backend/ride/src/CpfValidator.ts
@@ -14,14 +14,10 @@ export default class CpfValidator {
   }
 
   validate () {
-    try {
       this.cpfCalculator = new CpfCalculator(this.validCpf);
       const [firstDigit, secondDigit] = this.cpfCalculator.calculateDigits();
       const firstFinalDigit = this.cpfCalculator.calculateFirstFinalDigit(firstDigit);
       const secondFinalDigit = this.cpfCalculator.calculateSecondFinalDigit(secondDigit, firstFinalDigit);
       return this.cpfCalculator.isVerifierDigitValid(firstFinalDigit, secondFinalDigit);
-    } catch (e) {
-      throw new Error("An error occurred while validating CPF: " + e.message);
-    }
   }
 }
